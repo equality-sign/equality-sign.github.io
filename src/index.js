5e2a50dab2491969fe44fea311396e631000310c
mingrui <mingrui@gmail.com>
2020-09-19 04:36:16 +0800

    019001

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 $(function() {
+
     $('li:odd').css('backgroundColor', 'red')
-    $('li:even').css('backgroundColor', 'skyblue')
+    $('li:even').css('backgroundColor', 'blue')
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'
+})
+
 module.exports = {
-    'mode': 'development'
+    plugins: [htmlPlugin],
+    mode: 'development',
+    entry: path.join(__dirname, '/src/index.js'),
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'buldle.js'
+    }
 }
