5145091c9de06236d29011c9e47426cd362d1e58
mingrui <mingrui@gmail.com>
2020-09-19 11:42:46 +0800

    019003

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import $ from 'jquery'
+import './css/1.css'
+
 $(function() {
 
     $('li:odd').css('backgroundColor', 'red')
     $('li:even').css('backgroundColor', 'blue')
 })
+
+class Person {
+    static info = 'aaa'
+}
+console.log(Person.info);
--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,18 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'buldle.js'
+    },
+    module: {
+
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+            { test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/, use: 'url-loader?limit=16940' },
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+            // { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+            // { test: /\.vue$/, use: 'vue-loader' }
+        ]
     }
+
 }
